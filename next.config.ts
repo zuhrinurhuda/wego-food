7a5691936fe427fc6df7a2d34f445bb3376e68e8
zuhrinurhuda <zuhri.nurhuda@kitalulus.com>
2024-11-04 23:29:51 +0700

    chore: add wego image hostname

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["zen.wego.com"],
+  },
 };
 
 export default nextConfig;
