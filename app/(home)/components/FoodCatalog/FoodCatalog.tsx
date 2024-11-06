294aa44d2a6b0243b5de0282ece6465b50ccc462
zuhrinurhuda <zuhri.nurhuda@kitalulus.com>
2024-11-06 08:49:55 +0700

    feat: implement infinite scroll

--- app/(home)/components/FoodCatalog/FoodCatalog.tsx
+++ app/(home)/components/FoodCatalog/FoodCatalog.tsx
@@ -1,17 +1,34 @@
 "use client";
 
+import { useCallback, useState } from "react";
+
 import Card from "@/components/ui/Card";
+import Button from "@/components/ui/Button";
+
 import styles from "./FoodCatalog.module.css";
 import { useFood } from "../../context/FoodContext";
 
 export default function FoodCatalog() {
   const { filteredFoods } = useFood();
+  const [dataToShow, setDataToShow] = useState(9);
+  const [visibleData, setVisibleData] = useState(filteredFoods.slice(0, 9));
+
+  const handleShowMore = useCallback(() => {
+    const newDataToShow = dataToShow + 9;
+    setVisibleData(filteredFoods.slice(0, newDataToShow));
+    setDataToShow(newDataToShow);
+  }, [dataToShow, filteredFoods]);
 
   return (
-    <div className={styles.catalog}>
-      {filteredFoods.map((food) => (
-        <Card key={food?.id} {...food} />
-      ))}
-    </div>
+    <>
+      <div className={styles.catalog}>
+        {visibleData.map((food) => (
+          <Card key={food?.id} {...food} />
+        ))}
+      </div>
+      <Button className={styles.button} onClick={handleShowMore}>
+        + Show More
+      </Button>
+    </>
   );
 }
